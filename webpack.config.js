bf77e5151f2cdebb089281a86165a27c59eaeca7
ovodov_ii <pain3007@mail.ru>
2020-12-18 01:42:39 +0300

    build(common): add config file

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+    mode: `development`,
+    entry: `./src/main.js`,
+    output: {
+        filename: `bundle.js`,
+        path: path.join(__dirname, `public`)
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                exclude: '/node_modules/'
+            }, {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ],
+                exclude: '/node_modules/'
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ],
+    devtool: `source-map`,
+    devServer: {
+        contentBase: path.join(__dirname, `public`),
+        watchContentBase: true,
+        port: 9000,
+        historyApiFallback: true,
+        open: true
+    }
+}
